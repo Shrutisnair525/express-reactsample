be2a656957a2efe2f323ec2245ee28e41ec2990b
shruti <shruti@qburst.com>
2016-08-12 17:59:16 +0530

    first

---
+++ sample.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Footer extends React.Component{
+  render() {
+    return (
+      <div>
+        <h3>"hello"</h3>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Footer/>, document.getElementById('text1'));
